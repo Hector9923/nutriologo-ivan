2867fd4c21462ab3bfcfd822a1dc390ae1eee710
Hector Sanchez <h.manuel2311@hotmail.com>
2023-07-02 00:37:33 -0600

    starting home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
+import Head from 'next/head';
 import Image from 'next/image'
+import Logo from '../public/images/logo.jpg'
+import Background from '../public/images/home-vegetables-background.jpg'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const name = 'Iván Prieto Nutricionista';
+export const siteTitle = 'Iván Prieto Nutricionista';
 
 export default function Home() {
   return (
-    <main className='home-container'>
-      <nav>
-        HELLO WORLD
+    <div className='home-container'>
+      {/* <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="og:title" content={siteTitle} />
+      </Head> */}
+      <nav className='navbar'>
+        <div className='logo'>
+          <Image src={Logo} width={70} height={70}></Image>
+        </div>
+        <div>
+          Ivan Prieto Nutricionista
+        </div>
+        <div>
+          Tabla
+        </div>
       </nav>
-      <h1>
-        CAMBIA TU ALIMENTACION MENDIGO MARRANO -Ivan Prieto Nutricionista
-      </h1>
-    </main>
+      <nav className='body-container'>
+        <div className='home-title'>
+          Toma tu salud en tus propias manos
+        </div>
+        <div className='background-image'>
+          <Image src={Background} width={400} height={700}></Image>
+        </div>
+      </nav>
+    </div>
   )
 }
