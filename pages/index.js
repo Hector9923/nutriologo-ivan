fa99011dce85210164cffb222ae20cd2d7de9c1a
Hector Sanchez <h.manuel2311@hotmail.com>
2023-07-02 19:01:13 -0600

    skewed, bubbles and table

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head';
 import Image from 'next/image'
 import Logo from '../public/images/logo.jpg'
 import Background from '../public/images/home-vegetables-background.jpg'
+import steak from '../public/images/steak.jpg'
+import salad from '../public/images/salad.jpg'
+import berries from '../public/images/berries.jpg'
+import strawberries from '../public/images/strawberries.jpg'
 import { Inter } from 'next/font/google'
 
 const name = 'Iván Prieto Nutricionista';
@@ -14,25 +18,75 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="og:title" content={siteTitle} />
       </Head> */}
+      {/* Navigation bar */}
       <nav className='navbar'>
         <div className='logo'>
           <Image src={Logo} width={70} height={70}></Image>
         </div>
         <div>
-          Ivan Prieto Nutricionista
+          Iván Prieto | Nutriólogo
         </div>
         <div>
           Tabla
         </div>
       </nav>
-      <nav className='body-container'>
+
+      {/*Top section of landing page*/}
+      <div className='body-container'>
         <div className='home-title'>
           Toma tu salud en tus propias manos
         </div>
         <div className='background-image'>
           <Image src={Background} width={400} height={700}></Image>
         </div>
-      </nav>
+      </div>
+
+      {/* Picture bubbles section */}
+      <div className='picture-bubbles'>
+        <Image src={steak} className='bubble'></Image>
+        <Image src={salad} className='bubble'></Image>
+        <Image src={berries} className='bubble'></Image>
+        <Image src={strawberries} className='bubble'></Image>
+        {/* <Image src={} className='bubble'></Image> */}
+      </div>
+
+      {/* Table */}
+      <table className='styled-table'>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+            <td>Data 3</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 6</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 6</td>
+          </tr>
+          <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 6</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
